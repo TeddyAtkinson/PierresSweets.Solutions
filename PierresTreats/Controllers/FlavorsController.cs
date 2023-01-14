37490ce2077feac42e2632689f22102ba0a21c18
teddyatkinson <tedddy.atkinson@gmail.com>
2023-01-13 16:41:07 -0800

    Add flavor add treat method

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -65,5 +65,12 @@ namespace PierresTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddTreat(int id)
+    {
+      var chosenFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View(chosenFlavor);
+    }
   }
 }
